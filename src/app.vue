ff9dcd095d405215400e25aa485273733243d5a2
Jason011125 <zic022@ucsd.edu>
2023-03-02 23:20:16 -0800

    :tada: Initial commit

---
+++ src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>个人简历</h1>
+  <el-button>123</el-button>
+  <el-icon>
+    <i-ep-plus></i-ep-plus>
+  </el-icon>
+</template>
+
+
+<script setup>
+
+</script>
+
+<script>
+export default {
+  name: "app"
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
